800c5a127d606d217856de02c4b79d392d3159d1
hacking <western.ant2@gmail.com>
2022-08-29 19:27:22 +0100

    finished

--- results/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Result
-
-# Register your models here.
-
-class ResultAdmin(admin.ModelAdmin):
-    list_display = ('quiz', 'user', 'score',)
-
-admin.site.register(Result, ResultAdmin)
