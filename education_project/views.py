ef458463d48e36e7e9e6c2b255731df0db44f2b2
Den <deko@opinov8.com>
2022-09-25 21:31:25 +0100

    add initial templates

--- education_project/views.py
+++ education_project/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def render404(request):
     return render(request, '404.html')
+def main_page(request):
+    return render(request, 'main.html')
+def homework(request):
+    return render(request, 'homework.html')
+def homework_single(request):
+    return render(request, 'homework_single.html')
+def textbooks(request):
+    return render(request, 'textbooks.html')
+def methodical_work(request):
+    return render(request, 'methodical_work.html')
